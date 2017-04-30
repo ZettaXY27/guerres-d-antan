45bd3ffd463d45bf582900b7d9e735dc81afc8ad
ZettaXY27 <zettaxy27@gmail.com>
2017-04-30 19:44:13 -0400

    fixed template

--- src/zetta/CommandExec.java
+++ src/zetta/CommandExec.java
@@ -9,6 +9,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+@SuppressWarnings("unused")
 public class CommandExec implements CommandExecutor {
 	Main plugin;
 
@@ -19,32 +20,10 @@ public class CommandExec implements CommandExecutor {
 
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-	    if (cmd.getName().equalsIgnoreCase("untag")) {
-	        if (args.length != 1) {
-	            return false;
-	        }
-	        //Get the player to untag
-	        @SuppressWarnings("deprecation")
-			Player target = Bukkit.getServer().getPlayer(args[0]);
-
-	        // Make sure the player is online
-	        if (target == null) {
-	            sender.sendMessage(ChatColor.YELLOW + "[CombatTag++] " + ChatColor.GOLD + args[0] + " Has to be online to perform this command.");
-	            return true;
-	        }
-
-	        // Untags the player
-	        UUID uid = target.getUniqueId();
-	        if(plugin.getPlayerTags().getBoolean(uid + ".tagged") == true) {
-	        	plugin.getPlayerTags().set(uid + ".tagged", false);
-	        	plugin.savePlayerTags();
-	        	sender.sendMessage(ChatColor.YELLOW + "[CombatTag++] " + ChatColor.GOLD + args[0] + " has been untagged.");	
-	        	target.sendMessage(ChatColor.YELLOW + "[CombatTag++] " + ChatColor.GOLD + "You have been untagged");
-	        	return true;
-	        }
-	        return true;
-	    }
-	    return false;
+	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		// TODO yeah, actually add the command
+		return false;
 	}
+
+
 }
--- src/zetta/Main.java
+++ src/zetta/Main.java
@@ -26,165 +26,40 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
 
+@SuppressWarnings("unused")
 public class Main extends JavaPlugin implements Listener {
 	@Override
 	//debug messages and event registering
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);	
-		getPlayerTags();
+		getsecondConfig();
 		this.getCommand("untag").setExecutor(new CommandExec(this));
 	}
-	private  FileConfiguration playerTags = null;
-	private File playerTagsFile = null;
+	private  FileConfiguration secondConfig = null;
+	private File secondConfigFile = null;
 	
-	public void reloadPlayerTags() {
-	    if (playerTagsFile == null) {
-	    	playerTagsFile = new File(getDataFolder(), "playerTags.yml");
+	public void reloadsecondConfig() {
+	    if (secondConfigFile == null) {
+	    	secondConfigFile = new File(getDataFolder(), "secondConfig.yml");
 	    }
-	    playerTags = YamlConfiguration.loadConfiguration(playerTagsFile);
+	    secondConfig = YamlConfiguration.loadConfiguration(secondConfigFile);
 	}
-	public FileConfiguration getPlayerTags() {
-	    if (playerTags == null) {
-	    	reloadPlayerTags();
+	public FileConfiguration getsecondConfig() {
+	    if (secondConfig == null) {
+	    	reloadsecondConfig();
 	    }
-	    return playerTags;
+	    return secondConfig;
 	}
-	public void savePlayerTags() {
-	    if (playerTags == null || playerTagsFile == null) {
+	public void savesecondConfig() {
+	    if (secondConfig == null || secondConfigFile == null) {
 	        return;
 	    }
 	    try {
-	        getPlayerTags().save(playerTagsFile);
+	        getsecondConfig().save(secondConfigFile);
 	    } catch (IOException ex) {
-	        getLogger().log(Level.SEVERE, "Could not save config to " + playerTagsFile, ex);
+	        getLogger().log(Level.SEVERE, "Could not save config to " + secondConfigFile, ex);
 	    }
 	}
-	//Main onDamage event method that does all the tagging
-	@EventHandler
-	public void onDamage(EntityDamageByEntityEvent event) {                                      
-		if((event.getDamager() instanceof LivingEntity) || (event.getDamager() instanceof Arrow) || (event.getDamager() instanceof FallingBlock)  || (event.getDamager() instanceof LargeFireball) || (event.getDamager() instanceof SmallFireball)  || (event.getDamager() instanceof EnderPearl) && event.getEntity() instanceof Player) {
-		}
-		else {
-			if(event.getEntity() instanceof Player) {
-				Player player = (Player) event.getEntity();
-				java.util.UUID ID = player.getUniqueId();
-				if (player.hasPermission("cbtplusplus.immune")) {
-					getPlayerTags().set(ID + ".tagged", false); 
-					savePlayerTags();
-				}
-				else {
-					if(getPlayerTags().getBoolean(ID + ".tagged") == false ) {
-						if(player.getWorld().getName() == "SGworld" || (player.getWorld().getName() == "pa")) {
-							getPlayerTags().set(ID + ".tagged", false);
-							savePlayerTags();
-						}
-						getPlayerTags().set(ID + ".tagged", true);
-						savePlayerTags();
-						player.sendMessage(ChatColor.YELLOW + "[CombatTag++] " + ChatColor.GOLD + "You have been tagged! Do not log off or you will be punished.");
-						getLogger().info("Debug Info X001");
-						BukkitScheduler scheduler = getServer().getScheduler();
-						    scheduler.scheduleSyncDelayedTask(this, new Runnable() {
-						           @Override
-						           public void run() {
-						                // Scheduler does the stuff below here
-						   				if(getPlayerTags().getBoolean(ID + ".tagged") == true ) {
-						   					player.sendMessage(ChatColor.YELLOW + "[CombatTag++] " + ChatColor.GOLD +  "You can log off now.");
-							   				getPlayerTags().set(ID + ".tagged", false);
-							   				savePlayerTags();
-						   				}
-						   				else {
-						   					//Nothing here
-						   				}
-						           }
-						 }, /*Amount of ticks */ 400L);
-					}
-			}
-				//Not working currently (Attacker gets tag)
-				/*Player player2 = (Player) event.getDamager();
-				java.util.UUID ID2 = player2.getUniqueId();
-			    if (player2.hasPermission("cbtplusplus.immune")) {
-					getPlayerTags().set(ID2 + ".tagged", false); 
-					savePlayerTags();
-				}
-				else {
-					if(getPlayerTags().getBoolean(ID2 + ".tagged") == false ) {
-						getPlayerTags().set(ID2 + ".tagged", true);
-						savePlayerTags();
-						player2.sendMessage(ChatColor.YELLOW + "[CombatTag++] " + ChatColor.GOLD + "You have been tagged! Do not log off or you will be punished.");
-						getLogger().info("Debug Info X002");
-						BukkitScheduler scheduler = getServer().getScheduler();
-						    scheduler.scheduleSyncDelayedTask(this, new Runnable() {
-						           @Override
-						           public void run() {
-						                // Scheduler does the stuff below here
-						   				if(getPlayerTags().getBoolean(ID2 + ".tagged") == true ) {
-						   					player2.sendMessage(ChatColor.YELLOW + "[CombatTag++] " + ChatColor.GOLD +  "You can log off now.");
-							   				getPlayerTags().set(ID2 + ".tagged", false);
-							   				savePlayerTags();
-						   				}
-						   				else {
-						   					//Nothing here
-						   				}
-						           }*/
-						// }, /*Amount of ticks */ 400L);	
-						 
-				}
-			}
-		
-		}
-        //If the entity being damaged is a player
-
-				
-		/*if(event.getDamager() instanceof Player) {
-			if(event.getEntity() instanceof Player) {
-			}
-			else {
-				//Do nothing if its another type of entitee
-			}
-		
-		}
-		else {
-			// Do nothing if its another type of damage
-		}
-		*/		       		
-	@EventHandler
-	//Removes the attachment from the player when they log out, if they have it
-	public void onLogout(PlayerQuitEvent event) {
-		Player playerThatLoggedOut = event.getPlayer();
-		UUID logOutID = playerThatLoggedOut.getUniqueId();
-		if(getPlayerTags().getBoolean(logOutID + ".tagged") == true) {
-			Player player = event.getPlayer();
-			java.util.UUID ID = player.getUniqueId();
-				player.setHealth(0);
-				getPlayerTags().set(ID + ".tagged", false);
-				savePlayerTags();
-				this.getServer().broadcastMessage(player.getName() + " combat logged!");
-
-		}
-	}
-	@EventHandler
-	//Blocks teleportation
-	public void onTeleport(PlayerTeleportEvent event) {
-		Player playerThatTeleports = event.getPlayer();
-		UUID teleportID = playerThatTeleports.getUniqueId();
-		if(getPlayerTags().getBoolean(teleportID + ".tagged") == true) {
-			event.setCancelled(true);
-			event.getPlayer().sendMessage(ChatColor.YELLOW + "[CombatTag++] " + ChatColor.GOLD + "You are combat tagged! You can't teleport.");
-		}
-	}
-	@EventHandler
-	//Removes tag if player dies
-	public void onPDeath(PlayerDeathEvent event) {
-		Player playerThatDies = event.getEntity();
-		UUID deathID = playerThatDies.getUniqueId();
-		if(getPlayerTags().getBoolean(deathID + ".tagged") == true) {
-			getPlayerTags().set(deathID + ".tagged", false);
-			savePlayerTags();
-		}
-	}
-	
-	
-	
     @Override
     public void onDisable() {
     	getLogger().info("CombatTag++ has been disabled!");
