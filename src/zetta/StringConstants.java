ed77ac6274d69034876de8b67ad8f751ca2eb94d
sherendeen <inivican@hotmail.com>
2017-06-06 17:24:50 -0400

    Update StringConstants.java

--- src/zetta/StringConstants.java
+++ src/zetta/StringConstants.java
@@ -6,7 +6,13 @@ import org.bukkit.ChatColor;
  * 
  * @author Inivican
  * @since  Tuesday 6 June, 2017
- * 
+ *  
+ * Usage: StringConstants sc;
+ * then...
+ * sc.MESSAGE_PREFIX_MISTAKE + "..." etc
+ *
+ * Note: we do need to make the rest of the code fit to this model.
+ *
  */
 public class StringConstants {
 	public  final String MESSAGE_PREFIX_MISTAKE = ChatColor.GOLD + "["  + ChatColor.YELLOW + "GuerresD'Antan" + ChatColor.GOLD +"]" + ChatColor.RED;
