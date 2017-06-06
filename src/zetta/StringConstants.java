197c624dfc7c666b36f7deb1fc40a5f6a96d05e0
sherendeen <inivican@hotmail.com>
2017-06-06 17:22:58 -0400

    Created a class for storing the string constants for the facilitation of typing out messages.

---
+++ src/zetta/StringConstants.java
@@ -0,0 +1,19 @@
+package zetta;
+
+import org.bukkit.ChatColor;
+
+/**
+ * 
+ * @author Inivican
+ * @since  Tuesday 6 June, 2017
+ * 
+ */
+public class StringConstants {
+	public  final String MESSAGE_PREFIX_MISTAKE = ChatColor.GOLD + "["  + ChatColor.YELLOW + "GuerresD'Antan" + ChatColor.GOLD +"]" + ChatColor.RED;
+	public  final String MESSAGE_PREFIX_ERROR = ChatColor.GOLD + "["  + ChatColor.YELLOW + "GuerresD'Antan" + ChatColor.GOLD +"]" + ChatColor.DARK_RED;
+	public  final String MESSAGE_PREFIX_OK = ChatColor.GOLD + "["  + ChatColor.YELLOW + "GuerresD'Antan" + ChatColor.GOLD +"]" + ChatColor.GREEN;
+	public  final String MESSAGE_PREFIX_INFO = ChatColor.GOLD + "["  + ChatColor.YELLOW + "GuerresD'Antan" + ChatColor.GOLD +"]" + ChatColor.BLUE;
+	
+	
+	
+}
