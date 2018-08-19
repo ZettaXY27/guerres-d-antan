18d1678981bec8c1a587a7aa162928ad767f8574
Seth Herendeen <sherendeen16@fingerlakes.edu>
2018-08-18 23:39:29 -0400

    swiggity

--- src/gda2/Faction.java
+++ src/gda2/Faction.java
@@ -14,6 +14,40 @@ public class Faction {
 	private ArrayList<Faction> listOfEnemies;
 	private ArrayList<Faction> listOfAllies;
 	
+	private ArrayList<Member> memberList;
 	
 	
+	/**
+	 * Default constructor
+	 */
+	public Faction() {
+		
+	}
+	
+	/**
+	 * Creates the faction
+	 * @param factionName the name of the faction
+	 * @param userName the user's name of the player that created the faction.
+	 */
+	public Faction(String factionName, String userName) {
+		this.memberList = new ArrayList<Member>();
+		addMemberToMemberList(userName, Rank.EXECUTIVE);
+	}
+	
+	public void addMemberToMemberList(String userName) {
+		this.memberList.add(new Member(userName, Rank.CITIZEN));
+	}
+	
+	public void addMemberToMemberList(String userName, Rank rank) {
+		this.memberList.add(new Member(userName, rank));
+	}
+	
+	/**
+	 * Adds an entire list of members to the faction list. Maybe useful if there
+	 * is a case where a faction annexes another faction.
+	 * @param memberList
+	 */
+	public void addMembersToList(ArrayList<Member> memberList) {
+		this.memberList.addAll(memberList);
+	}
 }
