c5ce70b51f3689d43f0b4e8291cef01eb8d8b559
sherendeen <sherendeen@fingerlakes.edu>
2018-08-20 20:15:21 -0400

    Various
    + EventListener and stubs
    + UUID for member
    -  Removed dependence on player string name

--- src/gda2/Member.java
+++ src/gda2/Member.java
@@ -1,5 +1,7 @@
 package gda2;
 
+import java.util.UUID;
+
 /**
  * 
  * @author Inivican
@@ -9,18 +11,65 @@ package gda2;
 /* Defines the structure of a member of any given faction, their position
  * and ranking within the faction.
  */
-public class Member{
+public class Member {
 	//string name of the given player
 	private String name = "";
-	private Rank rankPositionWithinFaction;
+	private UUID uuid;
+	private Rank rank;
+	private String aestheticTitle = "";
 	
 	public Member() {
 		//Empty constructor
 	}
 	
-	public Member(String name, Rank rankPositionWithinFaction) {
+	/**
+	 * Instantiates member
+	 * @deprecated Use Member(UUID,Rank)
+	 * @param name
+	 * @param rank
+	 */
+	public Member(String name, Rank rank) {
 		this.name = name;
-		this.rankPositionWithinFaction = rankPositionWithinFaction;
+		this.rank = rank;
+	}
+	
+	/**
+	 * Instantiates Member
+	 * @param uuid
+	 * @param rank
+	 */
+	public Member(UUID uuid, Rank rank) {
+		this.uuid = uuid;
+		this.rank = rank;
+	}
+	
+	public void setAestheticTitle(String aestheticTitle) {
+		this.aestheticTitle = aestheticTitle;
+	}
+	
+	/**
+	 * gets the aesthetic title of the member
+	 * @return
+	 */
+	public String getAestheticTitle() {
+		return this.aestheticTitle;
+	}
+	
+	/**
+	 * Sets the rank of the member
+	 * @param rank the rank of the player as an enumeration
+	 */
+	public void setRank(Rank rank) {
+		this.rank = rank;
+	}
+	
+	
+	/**
+	 * gets the rank of the member
+	 * @return the rank as an enumerated type called Rank
+	 */
+	public Rank getRank() {
+		return this.rank;
 	}
 	
 }
